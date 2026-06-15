8ea57a9e5bd5dedf090b8bfc31bbb476b5d9d0f3
AdrianOrtizdelaCruz <adrian.ortiz@alumnos.uneatlantico.es>
2026-06-15 18:33:17 +0200

    feat: implement app.css/ app.html/ app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './shared/components/navbar/navbar';
-import { FooterComponent } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  template: `
-    <app-navbar />
-    <main class="main-content">
-      <router-outlet />
-    </main>
-    <app-footer />
-  `,
-  styles: [`
-    .main-content {
-      min-height: calc(100vh - 140px);
-      padding-top: 64px; /* navbar height */
-    }
-  `]
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
-export class AppComponent {}
+export class App {
+
+  title = 'Toybox';
+
+}
