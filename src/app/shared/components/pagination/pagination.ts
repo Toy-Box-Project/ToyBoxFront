09da22869b582e9f50a94e3351f7c8c64dfedfa9
AdrianOrtizdelaCruz <adrian.ortiz@alumnos.uneatlantico.es>
2026-06-16 12:17:38 +0200

    feat: implement "Pagination" component

--- src/app/shared/components/pagination/pagination.ts
+++ src/app/shared/components/pagination/pagination.ts
@@ -1,20 +1,40 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
   templateUrl: './pagination.html',
   styleUrl: './pagination.css'
 })
 export class PaginationComponent {
+
   @Input() currentPage = 1;
+
   @Input() totalPages = 1;
+
   @Output() pageChange = new EventEmitter<number>();
 
-  goToPage(page: number): void {
-    if (page >= 1 && page <= this.totalPages) {
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  previous(): void {
+    if (this.currentPage > 1) {
+      this.pageChange.emit(this.currentPage - 1);
+    }
+  }
+
+  next(): void {
+    if (this.currentPage < this.totalPages) {
+      this.pageChange.emit(this.currentPage + 1);
+    }
+  }
+
+  goTo(page: number): void {
+    if (page !== this.currentPage) {
       this.pageChange.emit(page);
     }
   }
+
 }
