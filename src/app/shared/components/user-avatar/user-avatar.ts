57cfec77b9f189dbffc2abd03391ea3652e9e02a
LunaLopez <luna.lopezdlf@gmail.com>
2026-06-19 22:00:23 +0200

    feat: add user-avatar component HTML CSS TS

--- src/app/shared/components/user-avatar/user-avatar.ts
+++ src/app/shared/components/user-avatar/user-avatar.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-avatar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-avatar.html',
   styleUrl: './user-avatar.css'
 })
-export class UserAvatarComponent {}
+export class UserAvatarComponent {
+
+  @Input() src: string | null = null;   // URL Cloudinary o null
+  @Input() name: string = '';           
+  @Input() size: number = 48;           
+
+  defaultAvatar: string = '../assets/images/default-avatar.png';
+
+  get finalSrc(): string | null {
+    // Si hay foto → Cloudinary
+    if (this.src && this.src.trim() !== '') return this.src;
+
+    // Si NO hay foto → imagen por defecto
+    return this.defaultAvatar;
+  }
+
+  get initials(): string {
+    if (!this.name) return '?';
+    return this.name
+      .split(' ')
+      .map(n => n[0])
+      .join('')
+      .toUpperCase()
+      .slice(0, 2);
+  }
+}
