90fc1565c2bfd787d9e2e73323511f69a97c6f8e
Suso <info@adenid.com>
2026-06-21 13:29:27 +0200

    feat: add product detail page layout

--- src/app/pages/product-detail/product-detail.ts
+++ src/app/pages/product-detail/product-detail.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css'
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent {
+  product: Product = {
+    id: 1,
+    title: 'Pack de coches y figuras',
+    category: 'Figuras, muñecos y vehículos',
+    price: 18,
+    location: 'Madrid',
+    status: 'Muy buen estado',
+    image: '/assets/images/Iconos%20categorias/icono_munecosycoches.svg',
+    badge: 'Publicado',
+    description:
+      'Pack de juguetes infantiles compuesto por coches, pequeñas figuras y accesorios. Está en muy buen estado y listo para que otro niño pueda seguir jugando.',
+    seller: {
+      name: 'María López',
+      rating: 4.8,
+      reviews: 23,
+      city: 'Madrid'
+    }
+  };
+
+  relatedProducts: Product[] = [
+    {
+      id: 2,
+      title: 'Caja de bloques',
+      category: 'Construcción y bloques',
+      price: 22,
+      location: 'Valencia',
+      status: 'Buen estado',
+      image: '/assets/images/Iconos%20categorias/icono_construccion.svg',
+      badge: 'Publicado'
+    },
+    {
+      id: 3,
+      title: 'Puzzle familiar',
+      category: 'Juegos de mesa y puzzles',
+      price: 12,
+      location: 'Murcia',
+      status: 'Como nuevo',
+      image: '/assets/images/Iconos%20categorias/icono_juegosmesa.svg',
+      badge: 'Publicado'
+    },
+    {
+      id: 4,
+      title: 'Juego educativo',
+      category: 'Juguetes educativos',
+      price: 15,
+      location: 'Madrid',
+      status: 'Muy buen estado',
+      image: '/assets/images/Iconos%20categorias/icono_educativo.svg',
+      badge: 'Publicado'
+    }
+  ];
+}
