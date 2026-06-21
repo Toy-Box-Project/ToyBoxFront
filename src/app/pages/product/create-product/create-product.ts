86488528f107465d9eaee99865e2c6f45cb675d4
Suso <info@adenid.com>
2026-06-21 14:16:27 +0200

    feat: add create product page layout

--- src/app/pages/product/create-product/create-product.ts
+++ src/app/pages/product/create-product/create-product.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Category } from '../../../shared/interfaces/category.interface';
 
 @Component({
   selector: 'app-create-product',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
   templateUrl: './create-product.html',
   styleUrl: './create-product.css'
 })
-export class CreateProductComponent {}
+export class CreateProductComponent {
+  categories: Category[] = [
+    {
+      id: 1,
+      name: 'Figuras, muñecos y vehículos',
+      icon: '/assets/images/Iconos%20categorias/icono_munecosycoches.svg'
+    },
+    {
+      id: 2,
+      name: 'Construcción y bloques',
+      icon: '/assets/images/Iconos%20categorias/icono_construccion.svg'
+    },
+    {
+      id: 3,
+      name: 'Juegos de mesa y puzzles',
+      icon: '/assets/images/Iconos%20categorias/icono_juegosmesa.svg'
+    },
+    {
+      id: 4,
+      name: 'Juguetes educativos',
+      icon: '/assets/images/Iconos%20categorias/icono_educativo.svg'
+    },
+    {
+      id: 5,
+      name: 'Bebés y primera infancia',
+      icon: '/assets/images/Iconos%20categorias/icono_bebes.svg'
+    },
+    {
+      id: 6,
+      name: 'Aire libre y movimiento',
+      icon: '/assets/images/Iconos%20categorias/icono_airelibre.svg'
+    },
+    {
+      id: 7,
+      name: 'Imaginación y juego simbólico',
+      icon: '/assets/images/Iconos%20categorias/icono_imaginacion.svg'
+    },
+    {
+      id: 8,
+      name: 'Videojuegos y consolas',
+      icon: '/assets/images/Iconos%20categorias/icono_videojuegos.svg'
+    }
+  ];
+
+  productStates: string[] = [
+    'Como nuevo',
+    'Muy buen estado',
+    'Buen estado',
+    'Usado'
+  ];
+}
