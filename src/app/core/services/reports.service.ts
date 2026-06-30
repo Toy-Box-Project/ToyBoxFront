a9c315b64f4887d698d35b5afa482f0953d5b4e2
Suso <info@adenid.com>
2026-06-30 22:43:57 +0200

    feat: add favorites and product report actions

--- src/app/core/services/reports.service.ts
+++ src/app/core/services/reports.service.ts
@@ -6,23 +6,31 @@ import { Report } from '../../shared/interfaces/report.interface';
 
 @Injectable({ providedIn: 'root' })
 export class ReportsService {
-  private readonly API = `${environment.apiUrl}/admin/reports`;
+  private readonly ADMIN_API = `${environment.apiUrl}/admin/reports`;
+  private readonly REPORTS_API = `${environment.apiUrl}/reports`;
 
   constructor(private http: HttpClient) {}
 
+  create(productId: number, reason: string): Observable<Report> {
+    return this.http.post<Report>(this.REPORTS_API, {
+      fk_items_id: productId,
+      reason
+    });
+  }
+
   getPending(): Observable<any> {
-    return this.http.get<any>(this.API);
+    return this.http.get<any>(this.ADMIN_API);
   }
 
   getById(id: number): Observable<Report> {
-    return this.http.get<Report>(`${this.API}/${id}`);
+    return this.http.get<Report>(`${this.ADMIN_API}/${id}`);
   }
 
   approve(productId: number): Observable<void> {
-    return this.http.patch<void>(`${this.API}/${productId}/approve`, {});
+    return this.http.patch<void>(`${this.ADMIN_API}/${productId}/approve`, {});
   }
 
   withdraw(productId: number): Observable<void> {
-    return this.http.patch<void>(`${this.API}/${productId}/withdraw`, {});
+    return this.http.patch<void>(`${this.ADMIN_API}/${productId}/withdraw`, {});
   }
 }
