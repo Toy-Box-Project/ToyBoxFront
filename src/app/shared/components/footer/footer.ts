bbcee83ff1248c80fdcd7ba5271105c3fc317007
JaimeC <jaimecovi@gmail.com>
2026-06-30 18:21:18 +0200

    feat: footer cambiado dependiendo de si estas registrado o no

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -9,6 +10,13 @@ import { RouterModule } from '@angular/router';
   templateUrl: './footer.html',
   styleUrls: ['./footer.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   currentYear: number = new Date().getFullYear();
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
 }
